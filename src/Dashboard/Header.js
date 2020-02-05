25471041cd2991293f39607b4713ad26d82e9222
unknown <thiago@instaletecnologia.com.br>
2020-02-05 18:25:46 -0300

    adição de parametros para mostrar origem, destino e ocorrencia

--- src/Dashboard/Header.js
+++ src/Dashboard/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import ModalConfig from '../_components/ModalConfig';
@@ -15,6 +15,7 @@ const Header = ({
   const [sector, setSector] = useState(
     localStorage.getItem('StatusMachine@filterSector') || '',
   );
+
 
   const handleModalOpened = () => setModalOpened(!modalOpened);
 
